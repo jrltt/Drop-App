292376826a428fbe6e4f203e920398a04f7573c9
Joaquin <joaquin@ptchwrks.com>
2016-01-14 09:49:06 +0100

    view for user/index, fix name controller

--- cloud/controllers/UserController.js
+++ cloud/controllers/UserController.js
@@ -4,7 +4,7 @@ var User = Parse.Object.extend('User');
 exports.index = function(req, res) {
   var query = new Parse.Query(User);
   query.find().then(function(results) {
-    res.render('users/index', { users : results});
+    res.render('user/index', { users : results});
   },
   function() {
     res.send(500, 'Fail loading user index');
