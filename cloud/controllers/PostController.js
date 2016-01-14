33e3a743380158b2bed6b6d06ff6c7dd1e539a5e
Joaquin <hola@jrltt.net>
2016-01-14 18:51:17 +0100

    add pointer on store method and show the author of a post

--- cloud/controllers/PostController.js
+++ cloud/controllers/PostController.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+
 var Post = Parse.Object.extend('Post');
 
 exports.index = function(req, res) {
@@ -17,11 +18,28 @@ exports.create = function(req, res) {
 
 exports.store = function(req, res) {
 	var post = new Post();
-	post.save(_.pick(req.body, 'title', 'content')).then(function() {
+	var title = req.body.title;
+	var content = req.body.content;
+	var currentUser = Parse.User.current();
+
+	post.set('title', title);
+	post.set('content', content);
+
+	// Set up ACL & Relation (pointer)
+	var acl = new Parse.ACL();
+	acl.setPublicReadAccess(true); // lectura publica
+	if ( currentUser ) { 
+		// Set (pointer) para asignar el usuario que ha creado el post
+		post.set('createdBy', currentUser);
+		acl.setWriteAccess(currentUser, true); // escritura solo este usuario
+	}
+	post.setACL(acl);
+
+	post.save().then(function() {
 		res.redirect('/post');
 	},
-	function() {
-		res.send(500, 'Failed saving post.');
+	function(error) {
+		res.json({code: error.code, message: error.message});
 	});
 };
 
@@ -54,7 +72,19 @@ exports.show = function(req, res) {
 	var postQuery = new Parse.Query(Post);
 	postQuery.get(req.params.id).then(function(post) {
 		if (post) {
-			res.render('../views/post/show', {post : post});
+			var query = new Parse.Query(Parse.User);
+			query.equalTo("objectId", post.get('createdBy').id);
+			query.find({
+			  success: function(results) {
+			  	console.log(results);
+			  	var username = 'test';
+			    res.render('../views/post/show', {post : post, user : username});
+			  },
+			  error: function(error) {
+			   res.render('../views/post/show', {post : post});
+			  }
+			});
+			// res.render('../views/post/show', {post : post, user : createdBy});
 		} else {
 			res.send('Not found');
 		}
